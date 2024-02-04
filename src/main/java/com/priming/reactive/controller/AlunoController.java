4e61a6f06f5d101f81a342c6b36d2635ab9b47de
JoseRFJuniorMedibands <144800596+JoseRFJuniorMedibands@users.noreply.github.com>
2024-02-04 05:26:25 +0000

    update aluno

--- src/main/java/com/priming/reactive/controller/AlunoController.java
+++ src/main/java/com/priming/reactive/controller/AlunoController.java
@@ -1,11 +1,16 @@
 package com.priming.reactive.controller;
 
 import com.priming.reactive.model.AlunoCollection;
+import com.priming.reactive.model.PrimeTargetFraseCollection;
+import com.priming.reactive.model.PrimeTargetTextCollection;
 import com.priming.reactive.service.AlunoService;
+import com.priming.reactive.service.PrimeTargetFraseService;
+import com.priming.reactive.service.PrimeTargetTextService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @CrossOrigin(origins = "http://localhost:8080")
 @RestController
@@ -13,14 +18,48 @@ import reactor.core.publisher.Flux;
 public class AlunoController {
 
     @Autowired
-    AlunoService alunoService;
+    private AlunoService alunoService;
+
+    @Autowired
+    private PrimeTargetFraseService primeTargetFraseService;
+
+    @Autowired
+    private PrimeTargetTextService primeTargetTextService;
 
     @GetMapping("/aluno")
     @ResponseStatus(HttpStatus.OK)
     public Flux<AlunoCollection> getAllAlunos(@RequestParam(required = false) String curso) {
-        if (curso == null)
+        if (curso == null) {
             return alunoService.findAll();
-        else
+        } else {
             return alunoService.findByCurso(curso);
+        }
+    }
+
+    @GetMapping("/aluno/target-info")
+    public Mono<AlunoTargetInfo> getAlunoTargetInfo(@RequestParam String target) {
+        Mono<PrimeTargetFraseCollection> fraseInfo = primeTargetFraseService.findTargetAndTextByPrime(target);
+        Mono<PrimeTargetTextCollection> textInfo = primeTargetTextService.findTargetAndTextByPrime(target);
+
+        return Mono.zip(fraseInfo, textInfo)
+                .map(tuple -> new AlunoTargetInfo(tuple.getT1(), tuple.getT2()));
+    }
+
+    public static class AlunoTargetInfo {
+        private final PrimeTargetFraseCollection fraseInfo;
+        private final PrimeTargetTextCollection textInfo;
+
+        public AlunoTargetInfo(PrimeTargetFraseCollection fraseInfo, PrimeTargetTextCollection textInfo) {
+            this.fraseInfo = fraseInfo;
+            this.textInfo = textInfo;
+        }
+
+        public PrimeTargetFraseCollection getFraseInfo() {
+            return fraseInfo;
+        }
+
+        public PrimeTargetTextCollection getTextInfo() {
+            return textInfo;
+        }
     }
 }
