303715a6ea5ecbdbae2d0fa6b106f8cb6f9a5bff
JoseRFJuniorMedibands <144800596+JoseRFJuniorMedibands@users.noreply.github.com>
2024-02-05 22:09:00 +0000

    update pom.xml

--- src/main/java/com/priming/reactive/model/core/PrimeTargetFraseCollection.java
+++ src/main/java/com/priming/reactive/model/core/PrimeTargetFraseCollection.java
@@ -5,6 +5,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
+import java.util.Objects;
 
 @Document(collection = "primeTargetFraseCollection")
 public class PrimeTargetFraseCollection {
@@ -17,6 +18,66 @@ public class PrimeTargetFraseCollection {
 
     private List<String> frase;
 
-    private String url;
+    private List<String> url;
 
+    public PrimeTargetFraseCollection(String id, String prime, String target, List<String> frase, List<String> url) {
+        this.id = id;
+        this.prime = prime;
+        this.target = target;
+        this.frase = frase;
+        this.url = url;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPrime() {
+        return prime;
+    }
+
+    public void setPrime(String prime) {
+        this.prime = prime;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public void setTarget(String target) {
+        this.target = target;
+    }
+
+    public List<String> getFrase() {
+        return frase;
+    }
+
+    public void setFrase(List<String> frase) {
+        this.frase = frase;
+    }
+
+    public List<String> getUrl() {
+        return url;
+    }
+
+    public void setUrl(List<String> url) {
+        this.url = url;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PrimeTargetFraseCollection that = (PrimeTargetFraseCollection) o;
+        return Objects.equals(id, that.id) && Objects.equals(prime, that.prime) && Objects.equals(target, that.target) && Objects.equals(frase, that.frase) && Objects.equals(url, that.url);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, prime, target, frase, url);
+    }
 }
--- src/main/java/com/priming/reactive/repository/core/PrimeTargetFraseRepository.java
+++ src/main/java/com/priming/reactive/repository/core/PrimeTargetFraseRepository.java
@@ -10,6 +10,6 @@ import reactor.core.publisher.Mono;
 public interface PrimeTargetFraseRepository extends ReactiveMongoRepository<PrimeTargetFraseCollection, String> {
     Mono<PrimeTargetFraseCollection> findByPrime(String prime);
 
-    @Query(value = "{ 'prime' : ?0 }", fields = "{ 'prime' : 1, 'target' : 1, 'text' : 1}")
+    @Query(value = "{ 'prime' : ?0 }", fields = "{ 'prime' : 1, 'target' : 1, 'url' : 1, 'frase' : 1}")
     Flux<PrimeTargetFraseCollection> findTargetAndTextByPrime(String prime);
 }
