cb4bcd2930ef1094ce40a01e46b8fa02bc463c66
JoseRFJuniorMedibands <144800596+JoseRFJuniorMedibands@users.noreply.github.com>
2024-02-06 06:36:29 +0000

    update pom.xml

--- src/main/java/com/priming/reactive/repository/core/PrimeTargetYoutubeRepository.java
+++ src/main/java/com/priming/reactive/repository/core/PrimeTargetYoutubeRepository.java
@@ -4,9 +4,11 @@ import com.priming.reactive.model.core.PrimeTargetYoutubeCollection;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface PrimeTargetYoutubeRepository extends ReactiveMongoRepository<PrimeTargetYoutubeCollection, String> {
 
+    Mono<PrimeTargetYoutubeCollection> findByPrime(String prime);
     @Query(value = "{ 'prime' : ?0 }", fields = "{ 'prime' : 1, 'target' : 1, 'url' : 1")
     Flux<PrimeTargetYoutubeCollection> findTargetAndUrlByPrime(String prime);
 
