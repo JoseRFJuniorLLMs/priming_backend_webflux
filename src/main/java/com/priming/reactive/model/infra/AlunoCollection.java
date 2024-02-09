600ddabad57609a7cf32edc10ac98ebe2a3596b6
JoseRFJuniorMedibands <144800596+JoseRFJuniorMedibands@users.noreply.github.com>
2024-02-09 23:32:04 +0000

    UPDATE: aluno service, couser service

--- src/main/java/com/priming/reactive/model/infra/AlunoCollection.java
+++ src/main/java/com/priming/reactive/model/infra/AlunoCollection.java
@@ -1,5 +1,12 @@
 package com.priming.reactive.model.infra;
 
+import com.priming.reactive.enums.converters.StatusConverter;
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.validation.constraints.NotNull;
+
+import com.priming.reactive.enums.Status;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -10,19 +17,31 @@ import java.util.Objects;
 public class AlunoCollection {
     @Id
     private String id;
-
+    @NotNull
     private String nome;
-
-    private String cpf;
-
+    @NotNull
+    private String tax_identification_number;
+    @NotNull
+    private String personal_identification_number;
+    @NotNull
+    private String login;
+    @NotNull
+    private String password;
+    @NotNull
+    @Column(length = 8, nullable = false)
+    @Convert(converter = StatusConverter.class)
+    private Status status = Status.ACTIVE;
     private List<String> curso;
-
     private List<String> prime;
 
-    public AlunoCollection(String id, String nome, String cpf, List<String> curso, List<String> prime) {
+    public AlunoCollection(String id, String nome, String tax_identification_number, String personal_identification_number, String login, String password, Status status, List<String> curso, List<String> prime) {
         this.id = id;
         this.nome = nome;
-        this.cpf = cpf;
+        this.tax_identification_number = tax_identification_number;
+        this.personal_identification_number = personal_identification_number;
+        this.login = login;
+        this.password = password;
+        this.status = status;
         this.curso = curso;
         this.prime = prime;
     }
@@ -43,12 +62,44 @@ public class AlunoCollection {
         this.nome = nome;
     }
 
-    public String getCpf() {
-        return cpf;
+    public String getTax_identification_number() {
+        return tax_identification_number;
     }
 
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
+    public void setTax_identification_number(String tax_identification_number) {
+        this.tax_identification_number = tax_identification_number;
+    }
+
+    public String getPersonal_identification_number() {
+        return personal_identification_number;
+    }
+
+    public void setPersonal_identification_number(String personal_identification_number) {
+        this.personal_identification_number = personal_identification_number;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
     }
 
     public List<String> getCurso() {
@@ -72,12 +123,12 @@ public class AlunoCollection {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         AlunoCollection that = (AlunoCollection) o;
-        return Objects.equals(id, that.id) && Objects.equals(nome, that.nome) && Objects.equals(cpf, that.cpf) && Objects.equals(curso, that.curso) && Objects.equals(prime, that.prime);
+        return Objects.equals(id, that.id) && Objects.equals(nome, that.nome) && Objects.equals(tax_identification_number, that.tax_identification_number) && Objects.equals(personal_identification_number, that.personal_identification_number) && Objects.equals(login, that.login) && Objects.equals(password, that.password) && status == that.status && Objects.equals(curso, that.curso) && Objects.equals(prime, that.prime);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, nome, cpf, curso, prime);
+        return Objects.hash(id, nome, tax_identification_number, personal_identification_number, login, password, status, curso, prime);
     }
 }
 
